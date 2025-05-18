9ca7420408c199070cc5ae10050052cc36bbdcda
Phong Tran <phong.tran@tuni.fi>
2025-05-18 19:27:01 +0300

    worked init version

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,10 +1,21 @@
-// kernel.cpp
+extern "C" void kernel_main();
+
+__attribute__((section(".multiboot")))
+const unsigned int multiboot_header[] = {
+    0x1BADB002,              // magic
+    0x0,                     // flags
+    (unsigned int)(0 - 0x1BADB002)  // checksum = -(magic + flags)
+};
+
 extern "C" void kernel_main() {
-    const char* str = "Hello from C++ kernel!";
+    const char* msg = "Hello from your C++ kernel!";
     char* video = (char*)0xb8000;
-    for (int i = 0; str[i] != '\0'; ++i) {
-        video[i * 2] = str[i];
-        video[i * 2 + 1] = 0x07; // white on black
+
+    for (int i = 0; msg[i]; ++i) {
+        video[i * 2] = msg[i];
+        video[i * 2 + 1] = 0x07;
     }
+
     while (1);
 }
+
