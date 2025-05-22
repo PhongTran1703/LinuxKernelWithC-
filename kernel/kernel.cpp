6e94df49580b14e78dfc3155b93b0304594b2c7f
Phong Tran <phong.tran@tuni.fi>
2025-05-22 18:33:57 +0300

    Revert "worked init version"
    This reverts commit 9ca7420408c199070cc5ae10050052cc36bbdcda.

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,9 +1,7 @@
 #include "keyboard.h"
 
 extern "C" void keyboard_handler();
-extern "C" void load_idt(); //declared in inline assembly
-
-extern "C" void kernel_main();
+extern "C" void load_idt(); // declared in inline assembly
 
 __attribute__((section(".multiboot")))
 const unsigned int multiboot_header[] = {
@@ -12,20 +10,17 @@ const unsigned int multiboot_header[] = {
     (unsigned int)(0 - 0x1BADB002)  // checksum = -(magic + flags)
 };
 
-
 extern "C" void kernel_main() {
-    const char* msg = "Hello from your C++ kernel!";
+    const char* str = "Hello from C++ kernel!";
     char* video = (char*)0xb8000;
-
-    for (int i = 0; msg[i]; ++i) {
-        video[i * 2] = msg[i];
-        video[i * 2 + 1] = 0x07;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        video[i * 2] = str[i];
+        video[i * 2 + 1] = 0x07; // white on black
     }
 
-    //Inittialize keyboard interrupt
-    load_idt(); //load dummy IDT with handler at IRQ1
+    // Initialize keyboard interrupt
+    load_idt(); // load dummy IDT with handler at IRQ1
     asm volatile("sti");
-
 
     while (1);
 }
